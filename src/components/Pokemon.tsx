3fbaa12bcaaca255eb43b0d3bddfb14411d22686
Emerson <emerson.day@gmail.com>
2023-05-15 17:13:54 -0700

    added select arrows

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,16 +1,28 @@
+import LeftArrow from './LeftArrow'
+import RightArrow from './RightArrow'
+
 type Props = {
- name: string
- img: string
+  name: string
+  img: string
 }
 
-function Pokemon({name, img}:Props) {
+function Pokemon({ name, img }: Props) {
   return (
-      <div className="p-8 mt-8">
-    <div className="">
-      <h2 className="text-xl font-bold">{name}</h2>
-      <img src={img} alt={name} />
+    <div className='p-8 mt-8'>
+      <div className=''>
+        <h2 className='text-xl font-bold'>{name}</h2>
+        <div className='flex'>
+          <div className='flex items-center min-h-full'>
+            <LeftArrow />
+          </div>
+
+          <img src={img} alt={name} />
+          <div className='flex items-center min-h-full'>
+            <RightArrow />
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 export default Pokemon
